fec3f99b0e6e76c09435fc465619d4e5d85cd2af
edvaldoljr <edvaldoluizj@gmail.com>
2022-02-03 15:12:38 -0300

    Aula 3 -  ExceptionCustomizada_2

---
+++ src/main/java/me/dio/web/aula3/DivisaoNaoExataException.java
@@ -0,0 +1,13 @@
+package me.dio.web.aula3;
+
+public class DivisaoNaoExataException extends Exception{
+
+    private int numerado;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerado, int denominador) {
+        super(message);
+        this.numerado = numerado;
+        this.denominador = denominador;
+    }
+}
